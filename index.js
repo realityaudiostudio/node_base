09e1605451e7faae7cc541953aaee63fe74cf1d1
Alan Jose Santo <realityaudiostudio@gmail.com>
2024-06-27 15:52:32 +0530

    day4eve

--- index.js
+++ index.js
@@ -1,16 +1,61 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
-let user = [{'name':'Alan JS','email':'alanjosesanto@outlook.com','phone':'8086350450','username':'an_alan_musical'},{'name':'SreeLakshmi','email':'sreesreelakshmi248@gmail.com','phone':'9496898901','username':'_sree_a_lakshmi_'}];
-let pricing = [{'Toothbrush':'Rs.50/-','Bucket':'Rs.100/-','Pears Soap':'Rs.300/-','Surf Excel':'Rs.10/-'}]
+// let user = [{'name':'Alan JS','email':'alanjosesanto@outlook.com','phone':'8086350450','username':'an_alan_musical'},{'name':'SreeLakshmi','email':'sreesreelakshmi248@gmail.com','phone':'9496898901','username':'_sree_a_lakshmi_'}];
+let userdata = [];
+// let userdata = [{'name':'Alan JS','email':'alanjosesanto@outlook.com','phone':'8086350450','username':'an_alan_musical'},{'name':'SreeLakshmi','email':'sreesreelakshmi248@gmail.com','phone':'9496898901','username':'_sree_a_lakshmi_'}];
 
-app.get('/users',function(req,res){
-    res.json(user);
+app.use(cors());
+app.use(express.json());
+
+
+// app.get('/users',function(req,res){
+//     res.json(user);
+// })
+
+
+
+app.post('/userdata',function(req,res){
+    console.log(req.body);
+    userdata.push(req.body);
+    res.json({success:true});
+});
+
+app.get('/userdata',function(req,res){
+    res.json(userdata);
 })
 
-app.get('/pricing',function(req,res){
-    res.json(pricing);
-})
+// app.post('/login', function(req,res){
+//     console.log(req.body);
+//     for(let i=0;i<userdata.length;i++)
+//         {
+//             if(userdata[i].userName==req.body.userName)
+//                 {
+//                     if(userdata[i].password==req.body.password)
+//                         {
+//                             return res.json(userdata[i]);
+                            
+//                         }
+//                 }
+//         }
+//         return res.json("user not found!");
+// })
+
+//my own code mwone
+
+app.post('/login',function(req,res){
+    console.log(req.body);
+    const {userName,password} = req.body;
+    const usr = userdata.find(u =>u.userName === userName && u.password===password);
+    if (usr)
+        {
+            res.json({success:true,usr});
+        }
+        else{
+            res.json({success:false,message:"Invalid USER or PASS"});
+        }
+});
 
 app.listen(4550,function() {
     console.log('Server is listening ... on port 4550');
